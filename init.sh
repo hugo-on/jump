8fdf6e3fc5504eedfec771255499d3b32a144050
hugo-on <101410426+hugo-on@users.noreply.github.com>
2022-11-19 22:00:43 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -13,13 +13,9 @@ mkdir -p /etc/caddy /usr/share/caddy
 # caddy file
 cat > /etc/caddy/Caddyfile << EOF
 :$PORT {
-	#root * /usr/share/caddy
+	root * /usr/share/caddy
 	file_server
 
-	reverse_proxy / {
-		to http://cloudreve.lee2333.com
-	}
-	
 	reverse_proxy $WSPATH {
 		to unix//etc/caddy/less
 	}
@@ -53,13 +49,38 @@ EOF
 
 # web index page
 cat > /usr/share/caddy/index.html << EOF
+<!DOCTYPE html>
 <html>
-  <head></head>
-  <title>Authentication required</title>
+<head>
+  <title>Webtools</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.staticfile.org/twitter-bootstrap/5.1.1/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.staticfile.org/twitter-bootstrap/5.1.1/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
 
-  <body>
-    <h1>Permission denied.</h1>
-  </body>
+<div class="container mt-3">
+  <h2>Login</h2>
+	<form action="">
+	  <div class="mb-3 mt-3">
+		<label for="email" class="form-label">Email:</label>
+		<input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
+	  </div>
+	  <div class="mb-3">
+		<label for="pwd" class="form-label">Password:</label>
+		<input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pswd">
+	  </div>
+	  <div class="form-check mb-3">
+		<label class="form-check-label">
+		  <input class="form-check-input" type="checkbox" name="remember"> Remember me
+		</label>
+	  </div>
+	  <button type="submit" class="btn btn-primary">Submit</button>
+	</form>
+</div>
+
+</body>
 </html>
 EOF
 
